f84ee9d07b2729a0f7304d28cbda604a93ba3883
Simon Renoult <simon.renoult@gmail.com>
2019-09-18 22:28:54 +0200

    GET /people

--- src/application.js
+++ src/application.js
@@ -2,6 +2,7 @@ const bodyParser = require("body-parser");
 const compression = require("compression");
 const cors = require("cors");
 const express = require("express");
+const faker = require("faker");
 const helmet = require("helmet");
 const path = require("path");
 const responseTime = require("response-time");
@@ -20,10 +21,6 @@ app.set("logger", logger);
 
 app.use(responseTime());
 app.use(middlewares.addRequestId);
-app.use(middlewares.addDedicatedLoggerToEachRequest);
-app.use(middlewares.logExitingResponse);
-app.use(middlewares.logIncomingRequest);
-app.use(middlewares.supportBoomInResponse);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
@@ -35,7 +32,50 @@ const swagger = apiRegistrar.registerAllApi(app);
 
 app.use("/documentation", swaggerUi.serve, swaggerUi.setup(swagger));
 
+const people = createConsultants();
+
+app.get("/people", (req, res) => {
+  return res.json(people);
+});
+
 app.use(middlewares.handleUnknownRoutes);
 app.use(middlewares.handleErrors);
 
 module.exports = app;
+
+function createConsultants(number = 100) {
+  const consultants = [];
+  for (let index = 0; index < number; index += 1) {
+    consultants.push(createConsultant());
+  }
+  return consultants;
+}
+
+function createConsultant() {
+  return {
+    first_name: faker.name.firstName(),
+    last_name: faker.name.lastName(),
+    level: randomLevel(),
+    salaire: randomSalaire()
+  };
+}
+
+function randomLevel() {
+  return randomInArray([
+    "consultant",
+    "consultant_confirme",
+    "consultant_senior",
+    "manager",
+    "manage_confirme"
+  ]);
+}
+
+function randomSalaire() {
+  const max = 80000.0;
+  const min = 40000.0;
+  return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+function randomInArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
